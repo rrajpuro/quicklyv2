30427be0ce596f02cdeee2ff10dc1abb91c30e41
sujaysreedhar <sujaysreedharg@gmail.com>
2023-04-24 22:26:53 +0000

    Created northboundv2

--- scripts/etcdshow.sh
+++ scripts/etcdshow.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-# etcdctl get "$1" --prefix | awk 'BEGIN { printf "%-25s%s\n", "Key", "Value" } { if (NR%2==1) { key=$0 } else { printf "%-25s%s\n", key, $0 } }'
-# echo "Key            Value"
-# etcdctl get "" --prefix | awk '{ if (NR%2==1) { key=$0; if (length(key)>max) { max=length(key) } } else { printf "%-*s %s\n", max, key, $0 } }' max=0 | column -t -s $'\t'
-if [[ -z "$1" ]]; then
+# Parse command line options
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -p|--prefix)
+        prefix="$2"
+        shift
+        shift
+        ;;
+        -r|--regex)
+        regex="$2"
+        shift
+        shift
+        ;;
+        *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+done
+
+# Set default values for prefix and regex if not provided
+if [[ -z "$prefix" ]]; then
     prefix=""
-else
-    prefix="$1"
 fi
 
-etcdctl get --prefix "$prefix" | awk 'BEGIN { printf "Key" "\t" "Value" "\n" "---" "\t" "-----" "\n" } { if (NR%2==1) { key=$0 } else { print key "\t" $0 } }' | column -t -s $'\t'
+if [[ -z "$regex" ]]; then
+    regex=".*"
+fi
+
+# Get keys from etcd and filter based on prefix and regex
+etcdctl get --prefix "$prefix" | awk 'BEGIN { printf "Key" "\t" "Value" "\n" "---" "\t" "-----" "\n" } { if (NR%2==1) { key=$0 } else { print key "\t" $0 } }' | column -t -s $'\t' | grep -E "$regex"
